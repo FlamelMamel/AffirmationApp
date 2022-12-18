1c61861f0f66f5338510b4a0b8898202055fdd0b
Flamel <flamelmamel@gmail.com>
2022-12-18 16:24:46 +0600

    Initial commit

---
+++ app/src/androidTest/java/com/example/affirmationapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,34 @@
+package com.example.affirmationapp
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun writeSomething() {
+        Espresso.onView(withId(R.id.item_title))
+            .perform(ViewActions.typeText("50.00"))
+    }
+}
---
+++ app/src/main/java/com/example/affirmationapp/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.example.affirmationapp.model
+
+data class Affirmation(val stringResourceId: Int)
